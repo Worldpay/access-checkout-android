a84a5cd8c5daffb2de0e7e2b91f9155c5ebbdcf9
Daniel Beddoe <daniel.beddoe@worldpay.com>
2019-07-24 11:09:29 +0100

    Attempting to fix flaky test where image rendering appears to sometimes happen after the espresso thread checks for the change of card brand icon

--- checkout/sample/src/androidTest/java/com/worldpay/access/checkout/matchers/BrandVectorImageNameMatcher.kt
+++ checkout/sample/src/androidTest/java/com/worldpay/access/checkout/matchers/BrandVectorImageNameMatcher.kt
@@ -4,13 +4,28 @@ import android.support.test.espresso.matcher.BoundedMatcher
 import android.view.View
 import android.widget.ImageView
 import com.worldpay.access.checkout.AbstractUITest.CardBrand
+import com.worldpay.access.checkout.logging.LoggingUtils.debugLog
 import com.worldpay.access.checkout.views.PANLayout
+import org.awaitility.Awaitility
 import org.hamcrest.Description
+import java.util.concurrent.TimeUnit
 
 internal class BrandVectorImageNameMatcher private constructor(private val expectedBrand: CardBrand) :
     BoundedMatcher<View, ImageView>(ImageView::class.java) {
     override fun matchesSafely(actualItem: ImageView): Boolean {
-        return (actualItem.getTag(PANLayout.CARD_TAG)) == expectedBrand.cardBrandName
+
+        var actualTag = ""
+        return try {
+            Awaitility.await().atMost(5, TimeUnit.SECONDS).until {
+                actualTag = getActualTag(actualItem)
+                debugLog("BrandVectorImageNameMatcher", "Checking actual image tag: $actualTag matches expected: $expectedBrand")
+                actualTag == expectedBrand.cardBrandName
+            }
+            true
+        } catch (ex: Exception) {
+            debugLog("BrandVectorImageNameMatcher", "Actual image tag: $actualTag does not match expected: $expectedBrand")
+            false
+        }
     }
 
     override fun describeTo(description: Description) {
@@ -19,6 +34,10 @@ internal class BrandVectorImageNameMatcher private constructor(private val expec
     }
 
     companion object {
+
+        fun getActualTag(actualItem: ImageView): String {
+            return actualItem.getTag(PANLayout.CARD_TAG).toString()
+        }
 
         fun withBrandVectorImageName(brand: CardBrand): BrandVectorImageNameMatcher {
             return BrandVectorImageNameMatcher(brand)
