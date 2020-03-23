63d5c0af21549d620f90cb36c9ac8967b28b021a
Charlie Whight <cdwhight@gmail.com>
2020-03-23 16:56:56 +0000

    US318370-aded sessions json file to upload script

--- scripts/upload_pact.sh
+++ scripts/upload_pact.sh
@@ -5,3 +5,8 @@ curl --fail --show-error -v -XPUT \-H "Content-Type: application/json" \
   -d@$PROJECT_LOCATION/$LIBRARY_MODULE/target/pacts/access-checkout-android-sdk-verified-tokens.json \
   -u $PACTBROKER_USERNAME:$PACTBROKER_PASSWORD \
   https://$PACTBROKER_URL/pacts/provider/verified-tokens/consumer/access-checkout-android-sdk/version/$PROJECT_VERSION
+
+curl --fail --show-error -v -XPUT \-H "Content-Type: application/json" \
+  -d@$PROJECT_LOCATION/$LIBRARY_MODULE/target/pacts/access-checkout-android-sdk-sessions.json \
+  -u $PACTBROKER_USERNAME:$PACTBROKER_PASSWORD \
+  https://$PACTBROKER_URL/pacts/provider/verified-tokens/consumer/access-checkout-android-sdk/version/$PROJECT_VERSION
