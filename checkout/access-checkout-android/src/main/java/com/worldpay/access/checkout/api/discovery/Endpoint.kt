a3daf520c589a6992ec14f809ebc518186b2a60e
Charlie Whight <cdwhight@gmail.com>
2020-04-03 09:54:50 +0100

    US332333-serializable endpoints

--- checkout/access-checkout-android/src/main/java/com/worldpay/access/checkout/api/discovery/Endpoint.kt
+++ checkout/access-checkout-android/src/main/java/com/worldpay/access/checkout/api/discovery/Endpoint.kt
@@ -1,8 +1,9 @@
 package com.worldpay.access.checkout.api.discovery
 
 import com.worldpay.access.checkout.api.serialization.LinkDiscoveryDeserializer
+import java.io.Serializable
 
-internal class Endpoint(val endpoint: String) {
+internal class Endpoint(val endpoint: String) : Serializable {
     fun getDeserializer(): LinkDiscoveryDeserializer {
         return LinkDiscoveryDeserializer(endpoint)
     }
