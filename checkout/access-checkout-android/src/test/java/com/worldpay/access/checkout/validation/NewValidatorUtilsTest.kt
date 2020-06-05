fa7c0b3590ffc774b7665100b3b1a6911d732f70
Charlie Whight <charlie.whight@worldpay.com>
2020-06-05 12:12:18 +0100

    US362714 update new validator utils tests

--- checkout/access-checkout-android/src/test/java/com/worldpay/access/checkout/validation/NewValidatorUtilsTest.kt
+++ checkout/access-checkout-android/src/test/java/com/worldpay/access/checkout/validation/NewValidatorUtilsTest.kt
@@ -2,6 +2,8 @@ package com.worldpay.access.checkout.validation
 
 import com.worldpay.access.checkout.api.configuration.CardValidationRule
 import com.worldpay.access.checkout.validation.NewValidatorUtils.getValidationResultFor
+import com.worldpay.access.checkout.validation.NewValidatorUtils.isNumeric
+import com.worldpay.access.checkout.validation.NewValidatorUtils.regexMatches
 import org.junit.Assert.*
 import org.junit.Test
 
@@ -9,68 +11,68 @@ class NewValidatorUtilsTest {
 
     @Test
     fun `given single numeric char then isNumeric should return true`() {
-        assertTrue(ValidatorUtils.isNumeric("1"))
+        assertTrue(isNumeric("1"))
     }
 
     @Test
     fun `given all numeric chars then isNumeric should return true`() {
-        assertTrue(ValidatorUtils.isNumeric("1234567890"))
+        assertTrue(isNumeric("1234567890"))
     }
 
     @Test
     fun `given all non-numeric chars then isNumeric should return false`() {
-        assertFalse(ValidatorUtils.isNumeric("ABCDEFGHI"))
+        assertFalse(isNumeric("ABCDEFGHI"))
     }
 
     @Test
     fun `given some non-numeric chars then isNumeric should return false`() {
-        assertFalse(ValidatorUtils.isNumeric("123ABC456"))
+        assertFalse(isNumeric("123ABC456"))
     }
 
     @Test
     fun `given empty then isNumeric should return false`() {
-        assertFalse(ValidatorUtils.isNumeric(""))
+        assertFalse(isNumeric(""))
     }
 
     @Test
     fun `given special chars then isNumeric should return false`() {
-        assertFalse(ValidatorUtils.isNumeric("@£%&*!"))
+        assertFalse(isNumeric("@£%&*!"))
     }
 
     @Test
     fun `given single numeric char with a leading space then isNumeric should return false`() {
-        assertFalse(ValidatorUtils.isNumeric(" 1"))
+        assertFalse(isNumeric(" 1"))
     }
 
     @Test
     fun `given all numeric chars with a leading space then isNumeric should return false`() {
-        assertFalse(ValidatorUtils.isNumeric(" 123"))
+        assertFalse(isNumeric(" 123"))
     }
 
     @Test
     fun `given single numeric char with a trailing space then isNumeric should return false`() {
-        assertFalse(ValidatorUtils.isNumeric("1 "))
+        assertFalse(isNumeric("1 "))
     }
 
     @Test
     fun `given all numeric chars with a trailing space then isNumeric should return false`() {
-        assertFalse(ValidatorUtils.isNumeric("123 "))
+        assertFalse(isNumeric("123 "))
     }
 
     @Test
     fun `given all numeric chars with a space in the middle then isNumeric should return false`() {
-        assertFalse(ValidatorUtils.isNumeric("1 4"))
+        assertFalse(isNumeric("1 4"))
     }
 
     // regexMatches
     @Test
     fun `given an empty regex then PAN should not match`() {
-        assertFalse(ValidatorUtils.regexMatches("", "1234"))
+        assertFalse(regexMatches("", "1234"))
     }
 
     @Test
     fun `given an empty PAN then PAN should not match against regex`() {
-        assertFalse(ValidatorUtils.regexMatches("^1234$", ""))
+        assertFalse(regexMatches("^1234$", ""))
     }
 
     @Test
@@ -78,7 +80,7 @@ class NewValidatorUtilsTest {
         val pan = "11111111111111"
         val regex = "^1\\d{0,13}$"
 
-        assertTrue(ValidatorUtils.regexMatches(regex, pan))
+        assertTrue(regexMatches(regex, pan))
     }
 
     @Test
@@ -86,7 +88,7 @@ class NewValidatorUtilsTest {
         val pan = "111111111111112"
         val regex = "^1\\d{0,13}$"
 
-        assertFalse(ValidatorUtils.regexMatches(regex, pan))
+        assertFalse(regexMatches(regex, pan))
     }
 
     @Test
@@ -94,7 +96,7 @@ class NewValidatorUtilsTest {
         val pan = "21111111111111"
         val regex = "^1\\d{0,13}$"
 
-        assertFalse(ValidatorUtils.regexMatches(regex, pan))
+        assertFalse(regexMatches(regex, pan))
     }
 
     @Test
@@ -102,7 +104,7 @@ class NewValidatorUtilsTest {
         val pan = "1abcdefghijklm"
         val regex = "^1\\d{0,13}$"
 
-        assertFalse(ValidatorUtils.regexMatches(regex, pan))
+        assertFalse(regexMatches(regex, pan))
     }
 
     @Test
@@ -113,16 +115,16 @@ class NewValidatorUtilsTest {
         val panSuffix = "0000000"
         val regex = "^($panPrefix1|$panPrefix2|$panPrefix3)\\d{0,7}"
 
-        assertTrue(ValidatorUtils.regexMatches(regex, panPrefix1 + panSuffix))
-        assertTrue(ValidatorUtils.regexMatches(regex, panPrefix2 + panSuffix))
-        assertTrue(ValidatorUtils.regexMatches(regex, panPrefix3 + panSuffix))
+        assertTrue(regexMatches(regex, panPrefix1 + panSuffix))
+        assertTrue(regexMatches(regex, panPrefix2 + panSuffix))
+        assertTrue(regexMatches(regex, panPrefix3 + panSuffix))
     }
 
     @Test
     fun `given a PAN which does not matches in an OR pattern then should return false`() {
         val regex = "^(413600|444509|444550)\\d{0,7}"
 
-        assertFalse(ValidatorUtils.regexMatches(regex, "4136010000000"))
+        assertFalse(regexMatches(regex, "4136010000000"))
     }
 
     @Test
@@ -132,15 +134,15 @@ class NewValidatorUtilsTest {
         val panSuffix = "0000000000000"
         val regex = "^3[47]\\d{0,13}\$"
 
-        assertTrue(ValidatorUtils.regexMatches(regex, panPrefix1 + panSuffix))
-        assertTrue(ValidatorUtils.regexMatches(regex, panPrefix2 + panSuffix))
+        assertTrue(regexMatches(regex, panPrefix1 + panSuffix))
+        assertTrue(regexMatches(regex, panPrefix2 + panSuffix))
     }
 
     @Test
     fun `given a PAN which does not match in a group pattern then should return false`() {
         val regex = "^3[47]\\d{0,13}\$"
 
-        assertFalse(ValidatorUtils.regexMatches(regex, "350000000"))
+        assertFalse(regexMatches(regex, "350000000"))
     }
 
     @Test
