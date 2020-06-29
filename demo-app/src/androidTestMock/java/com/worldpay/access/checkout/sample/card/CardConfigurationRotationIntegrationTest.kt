62f7821918e58af0a7a2e8871604faca8fb7c603
Charlie Whight <charlie.whight@worldpay.com>
2020-06-29 11:28:49 +0100

    US366445 remove check for unkown validation state

--- demo-app/src/androidTestMock/java/com/worldpay/access/checkout/sample/card/CardConfigurationRotationIntegrationTest.kt
+++ demo-app/src/androidTestMock/java/com/worldpay/access/checkout/sample/card/CardConfigurationRotationIntegrationTest.kt
@@ -2,7 +2,6 @@ package com.worldpay.access.checkout.sample.card
 
 import com.worldpay.access.checkout.sample.MainActivity
 import com.worldpay.access.checkout.sample.card.testutil.AbstractCardFragmentTest
-import com.worldpay.access.checkout.sample.card.testutil.CardBrand
 import com.worldpay.access.checkout.sample.card.testutil.CardBrand.MASTERCARD
 import com.worldpay.access.checkout.sample.card.testutil.CardFragmentTestUtils
 import com.worldpay.access.checkout.sample.testutil.UITestUtils.rotateLandscape
@@ -19,56 +18,15 @@ class CardConfigurationRotationIntegrationTest: AbstractCardFragmentTest() {
 
     @Test
     fun givenScreenIsRotated_ThenFieldsShouldKeepValidationState() {
-        // Enter a partial pan and check the validation state has not been updated
-        CardFragmentTestUtils(activityRule)
-            .isInInitialState()
-            .enterCardDetails(pan = "4111")
-            .validationStateIsUnknown(pan = true)
-            .hasBrand(CardBrand.VISA)
-            .enabledStateIs(submitButton = false)
-
-        rotateLandscape(activityRule)
-
-        // Check no change on rotation
-        CardFragmentTestUtils(activityRule)
-            .validationStateIsUnknown(pan = true)
-            .hasBrand(CardBrand.VISA)
-            .enabledStateIs(submitButton = false)
-
-        // Enter a partial expiry date and check the validation state has not been updated
-        CardFragmentTestUtils(activityRule)
-            .enterCardDetails(expiryDate = "12")
-            .validationStateIsUnknown(expiryDate = true)
-            .enabledStateIs(submitButton = false)
-
-        rotatePortrait(activityRule)
-
-        // Check no change on rotation
-        CardFragmentTestUtils(activityRule)
-            .validationStateIsUnknown(expiryDate = true)
-            .enabledStateIs(submitButton = false)
-
-        // Enter a partial cvc and check the validation state has not been updated
-        CardFragmentTestUtils(activityRule)
-            .enterCardDetails(cvc = "11")
-            .validationStateIsUnknown(cvc = true)
-            .enabledStateIs(submitButton = false)
-
-        rotateLandscape(activityRule)
-
-        // Check no change on rotation
-        CardFragmentTestUtils(activityRule)
-            .validationStateIsUnknown(cvc = true)
-            .enabledStateIs(submitButton = false)
-
         // Enter an invalid luhn, mastercard identified card and valid date
         CardFragmentTestUtils(activityRule)
+            .isInInitialState()
             .enterCardDetails(pan = luhnInvalidMastercardCard, cvc = unknownCvc, expiryDate = "1299")
             .validationStateIs(pan = false, cvc = false, expiryDate = true)
             .hasBrand(MASTERCARD)
             .enabledStateIs(submitButton = false)
 
-        rotatePortrait(activityRule)
+        rotateLandscape(activityRule)
 
         // Re-enter a luhn valid, mastercard identified card and valid date
         CardFragmentTestUtils(activityRule)
@@ -81,7 +39,7 @@ class CardConfigurationRotationIntegrationTest: AbstractCardFragmentTest() {
             .validationStateIs(pan = true, cvc = true, expiryDate = true)
             .enabledStateIs(submitButton = true)
 
-        rotateLandscape(activityRule)
+        rotatePortrait(activityRule)
 
         CardFragmentTestUtils(activityRule)
             .hasBrand(MASTERCARD)
@@ -92,7 +50,7 @@ class CardConfigurationRotationIntegrationTest: AbstractCardFragmentTest() {
             .validationStateIs(pan = true, cvc = false, expiryDate = true)
             .enabledStateIs(submitButton = false)
 
-        rotatePortrait(activityRule)
+        rotateLandscape(activityRule)
 
         CardFragmentTestUtils(activityRule)
             .hasBrand(MASTERCARD)
