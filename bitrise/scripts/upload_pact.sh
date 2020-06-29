0ea19b1ded7a3df7f7f16c78046b959be78ac8e8
Charlie Whight <charlie.whight@worldpay.com>
2020-06-29 17:00:54 +0100

    US349831 update consumer name to android

--- bitrise/scripts/upload_pact.sh
+++ bitrise/scripts/upload_pact.sh
@@ -7,9 +7,9 @@ export VT_CONTRACT_VERSION=$PROJECT_VERSION
 curl --fail --show-error -v -XPUT \-H "Content-Type: application/json" \
   -d@$LIBRARY_MODULE/target/pacts/access-checkout-sdk-verified-tokens.json \
   -u $PACTBROKER_USERNAME:$PACTBROKER_PASSWORD \
-  https://$PACTBROKER_URL/pacts/provider/verified-tokens/consumer/access-checkout-sdk/version/$VT_CONTRACT_VERSION
+  https://$PACTBROKER_URL/pacts/provider/verified-tokens/consumer/access-checkout-android-sdk/version/$VT_CONTRACT_VERSION
 
 curl --fail --show-error -v -XPUT \-H "Content-Type: application/json" \
   -d@$LIBRARY_MODULE/target/pacts/access-checkout-sdk-sessions.json \
   -u $PACTBROKER_USERNAME:$PACTBROKER_PASSWORD \
-  https://$PACTBROKER_URL/pacts/provider/sessions/consumer/access-checkout-sdk/version/$SESSIONS_CONTRACT_VERSION
+  https://$PACTBROKER_URL/pacts/provider/sessions/consumer/access-checkout-android-sdk/version/$SESSIONS_CONTRACT_VERSION
