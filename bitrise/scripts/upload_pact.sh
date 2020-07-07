8171fd52aefea5c9a5c5179ae5459a69422fa073
Charlie Whight <charlie.whight@worldpay.com>
2020-07-07 15:18:56 +0100

    US349841 add git commit hash to pact version

--- bitrise/scripts/upload_pact.sh
+++ bitrise/scripts/upload_pact.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 set -e
 
+export HASH_CODE=$(git rev-parse --short HEAD)
 export SESSIONS_CONTRACT_VERSION="$PROJECT_VERSION"
 export VT_CONTRACT_VERSION=$PROJECT_VERSION
 
 curl --fail --show-error -v -XPUT \-H "Content-Type: application/json" \
   -d@$LIBRARY_MODULE/target/pacts/access-checkout-android-sdk-verified-tokens.json \
   -u $PACTBROKER_USERNAME:$PACTBROKER_PASSWORD \
-  https://$PACTBROKER_URL/pacts/provider/verified-tokens/consumer/access-checkout-android-sdk/version/$VT_CONTRACT_VERSION
+  https://$PACTBROKER_URL/pacts/provider/verified-tokens/consumer/access-checkout-android-sdk/version/$VT_CONTRACT_VERSION+$HASH_CODE
 
 curl --fail --show-error -v -XPUT \-H "Content-Type: application/json" \
   -d@$LIBRARY_MODULE/target/pacts/access-checkout-android-sdk-sessions.json \
   -u $PACTBROKER_USERNAME:$PACTBROKER_PASSWORD \
-  https://$PACTBROKER_URL/pacts/provider/sessions/consumer/access-checkout-android-sdk/version/$SESSIONS_CONTRACT_VERSION
+  https://$PACTBROKER_URL/pacts/provider/sessions/consumer/access-checkout-android-sdk/version/$SESSIONS_CONTRACT_VERSION+$HASH_CODE
