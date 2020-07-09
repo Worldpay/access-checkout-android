8cf32924c8ca059d7f6c587b02029120f8d4a58e
Charlie Whight <charlie.whight@worldpay.com>
2020-07-09 16:57:52 +0100

    US349840 fetch branch name from detached HEAD

--- bitrise/scripts/upload_pact.sh
+++ bitrise/scripts/upload_pact.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 set -e
 
-export HASH_CODE=$(git rev-parse --short HEAD)
-export BRANCH=$(git rev-parse --abbrev-ref HEAD)
+HASH_CODE=$(git rev-parse --short HEAD)
+BRANCH=$(\
+  git for-each-ref \
+  --format='%(objectname) %(refname:short)' refs/heads \
+  | awk "/^$(git rev-parse HEAD)/ {print \$2}"\
+)
 
-export PROJECT_VERSION="$PROJECT_VERSION"
+PROJECT_VERSION="$PROJECT_VERSION"
 
 curl --fail --show-error -v -XPUT \-H "Content-Type: application/json" \
   -d@$LIBRARY_MODULE/target/pacts/access-checkout-android-sdk-verified-tokens.json \
@@ -22,8 +26,8 @@ then
   curl --fail --show-error -v -XPUT \-H "Content-Type: application/json" \
   -u $PACTBROKER_USERNAME:$PACTBROKER_PASSWORD \
   https://$PACTBROKER_URL/pacticipants/access-checkout-android-sdk/versions/$PROJECT_VERSION+$HASH_CODE/tags/$BRANCH
+else
+  echo "Did not tag as on branch " $BRANCH
 fi
 
-echo "TEST"
-echo $BRANCH
 
