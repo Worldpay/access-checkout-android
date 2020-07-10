07fa2593afa674a6dceff86cdee074b731daf879
Charlie Whight <charlie.whight@worldpay.com>
2020-07-10 15:07:35 +0100

    US349833 verify prod tag on deployment

--- bitrise/scripts/tag_prod_pact_version.sh
+++ bitrise/scripts/tag_prod_pact_version.sh
@@ -4,3 +4,12 @@ set -e
 HASH_CODE=$(git rev-parse --short HEAD)
 
 pact-broker create-version-tag --pacticipant access-checkout-android-sdk --version $PROJECT_VERSION+$HASH_CODE --tag PROD --broker-base-url https://$PACTBROKER_URL -u $PACTBROKER_URL -p $PACTBROKER_PASSWORD
+
+DESCRIPTION=$(pact-broker describe-version --pacticipant access-checkout-android-sdk --version $PROJECT_VERSION+$HASH_CODE --broker-base-url https://$PACTBROKER_URL -u $PACTBROKER_URL -p $PACTBROKER_PASSWORD)
+if [[ $DESCRIPTION == *"PROD"* ]]
+then
+  echo "Successfully tagged as PROD"
+else
+  echo "Broker not successfully tagged"
+  exit 1
+fi
