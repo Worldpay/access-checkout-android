eff2fde76ce46247b34e40acadc7a101bd678ef7
Charlie Whight <charlie.whight@worldpay.com>
2020-07-13 15:57:23 +0100

    US349833 lower case prod

--- bitrise/scripts/tag_prod_pact_version.sh
+++ bitrise/scripts/tag_prod_pact_version.sh
@@ -3,14 +3,14 @@ set -e
 
 HASH_CODE=$(git rev-parse --short HEAD)
 
-echo "Tagging with PROD..."
-pact-broker create-version-tag --pacticipant access-checkout-android-sdk --version $PROJECT_VERSION+$HASH_CODE --tag PROD --broker-base-url https://$PACTBROKER_URL -u $PACTBROKER_USERNAME -p $PACTBROKER_PASSWORD
+echo "Tagging with prod..."
+pact-broker create-version-tag --pacticipant access-checkout-android-sdk --version $PROJECT_VERSION+$HASH_CODE --tag prod --broker-base-url https://$PACTBROKER_URL -u $PACTBROKER_USERNAME -p $PACTBROKER_PASSWORD
 
 echo "Verifying successfully tagged..."
 DESCRIPTION=$(pact-broker describe-version --pacticipant access-checkout-android-sdk --version $PROJECT_VERSION+$HASH_CODE --broker-base-url https://$PACTBROKER_URL -u $PACTBROKER_USERNAME -p $PACTBROKER_PASSWORD)
-if [[ $DESCRIPTION == *"PROD"* ]]
+if [[ $DESCRIPTION == *"prod"* ]]
   then
-    echo "Successfully tagged as PROD"
+    echo "Successfully tagged as prod"
   else
     echo "Failed as broker not successfully tagged"
     exit 1
