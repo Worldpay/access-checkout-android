0caa7d97a9d2eb96ff800751e922bb92d2850aef
junedmiah <miahj13@gmail.com>
2021-01-07 14:49:31 +0000

    US431035: Refactored the upload_pact.sh

--- bitrise/scripts/upload_pact.sh
+++ bitrise/scripts/upload_pact.sh
@@ -2,24 +2,33 @@
 set -e
 
 HASH_CODE=$(git rev-parse --short HEAD)
+VERSION="$PROJECT_VERSION+$HASH_CODE"
+VT_FILE=${LIBRARY_MODULE}/build/pacts/access-checkout-android-sdk-verified-tokens.json
+SESSIONS_FILE=${LIBRARY_MODULE}/build/pacts/access-checkout-android-sdk-sessions.json
+VT_URL=https://${PACTBROKER_URL}/pacts/provider/verified-tokens/consumer/access-checkout-android-sdk/version/${VERSION}
+SESSIONS_URL=https://${PACTBROKER_URL}/pacts/provider/sessions/consumer/access-checkout-android-sdk/version/${VERSION}
 
-PROJECT_VERSION="$PROJECT_VERSION"
+echo "Uploading pact file for verified tokens { file = ${VT_FILE} , version = ${VERSION} , url = ${VT_URL}}"
+curl --fail --show-error -v -X PUT \
+  -H "Content-Type: application/json" \
+  -d@"${VT_FILE}" \
+  -u "${PACTBROKER_USERNAME}:${PACTBROKER_PASSWORD}" \
+  "${VT_URL}"
 
-curl --fail --show-error -v -XPUT \-H "Content-Type: application/json" \
-  -d@$LIBRARY_MODULE/target/pacts/access-checkout-android-sdk-verified-tokens.json \
-  -u $PACTBROKER_USERNAME:$PACTBROKER_PASSWORD \
-  https://$PACTBROKER_URL/pacts/provider/verified-tokens/consumer/access-checkout-android-sdk/version/$PROJECT_VERSION+$HASH_CODE
+echo ""
+echo ""
 
-curl --fail --show-error -v -XPUT \-H "Content-Type: application/json" \
-  -d@$LIBRARY_MODULE/target/pacts/access-checkout-android-sdk-sessions.json \
-  -u $PACTBROKER_USERNAME:$PACTBROKER_PASSWORD \
-  https://$PACTBROKER_URL/pacts/provider/sessions/consumer/access-checkout-android-sdk/version/$PROJECT_VERSION+$HASH_CODE
+echo "Uploading pact file for sessions { file = ${SESSIONS_FILE} , version = ${VERSION} , url = ${SESSIONS_URL}}"
+curl --fail --show-error -v -X PUT \
+  -H "Content-Type: application/json" \
+  -d@"${SESSIONS_FILE}" \
+  -u "${PACTBROKER_USERNAME}:${PACTBROKER_PASSWORD}" \
+  "${SESSIONS_URL}"
 
-
-if [ $BITRISE_GIT_BRANCH == "master" ]
+if [ "${BITRISE_GIT_BRANCH}" == "master" ]
   then
     echo "Tagging broker with master..."
-    pact-broker create-version-tag --pacticipant access-checkout-android-sdk --version $PROJECT_VERSION+$HASH_CODE --tag $BITRISE_GIT_BRANCH --broker-base-url https://$PACTBROKER_URL -u $PACTBROKER_USERNAME -p $PACTBROKER_PASSWORD
+    pact-broker create-version-tag --pacticipant access-checkout-android-sdk --version "${VERSION}" --tag "${BITRISE_GIT_BRANCH}" --broker-base-url "https://${PACTBROKER_URL}" -u "${PACTBROKER_USERNAME}" -p "${PACTBROKER_PASSWORD}"
 fi
 
 
