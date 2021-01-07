0c99ee2c2ae0d6f138fe4fb9ee08472816b5c42f
junedmiah <miahj13@gmail.com>
2021-01-07 16:44:19 +0000

    US431035: Fixed all integration android tests

---
+++ access-checkout/src/androidTest/java/com/worldpay/access/checkout/api/ssl/server/CustomHttpServerFactory.kt
@@ -0,0 +1,24 @@
+package com.worldpay.access.checkout.api.ssl.server
+
+import com.github.tomakehurst.wiremock.core.Options
+import com.github.tomakehurst.wiremock.http.AdminRequestHandler
+import com.github.tomakehurst.wiremock.http.HttpServer
+import com.github.tomakehurst.wiremock.http.HttpServerFactory
+import com.github.tomakehurst.wiremock.http.StubRequestHandler
+
+class CustomHttpServerFactory : HttpServerFactory {
+
+    override fun buildHttpServer(
+        options: Options,
+        adminRequestHandler: AdminRequestHandler,
+        stubRequestHandler: StubRequestHandler
+    ): HttpServer {
+        return CustomHttpServer(
+            options,
+            adminRequestHandler,
+            stubRequestHandler
+        )
+    }
+
+
+}
