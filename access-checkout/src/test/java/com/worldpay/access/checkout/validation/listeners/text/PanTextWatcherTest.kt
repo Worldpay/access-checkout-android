da7e5b40ac86a699c3fca2115e68e62571873b6d
junedmiah <miahj13@gmail.com>
2021-01-19 12:08:50 +0000

    US426266: Updated the tests and replaced actual instances with mocks

--- access-checkout/src/test/java/com/worldpay/access/checkout/validation/listeners/text/PanTextWatcherTest.kt
+++ access-checkout/src/test/java/com/worldpay/access/checkout/validation/listeners/text/PanTextWatcherTest.kt
@@ -3,9 +3,9 @@ package com.worldpay.access.checkout.validation.listeners.text
 import android.text.Editable
 import android.widget.EditText
 import com.nhaarman.mockitokotlin2.*
-import com.worldpay.access.checkout.api.configuration.DefaultCardRules.CVC_DEFAULTS
 import com.worldpay.access.checkout.testutils.CardConfigurationUtil.Brands.VISA_BRAND
 import com.worldpay.access.checkout.testutils.CardConfigurationUtil.mockSuccessfulCardConfiguration
+import com.worldpay.access.checkout.testutils.CardNumberUtil.INVALID_UNKNOWN_LUHN
 import com.worldpay.access.checkout.testutils.CardNumberUtil.VALID_UNKNOWN_LUHN
 import com.worldpay.access.checkout.testutils.CardNumberUtil.VISA_PAN
 import com.worldpay.access.checkout.validation.result.handler.BrandChangedHandler
@@ -15,12 +15,11 @@ import com.worldpay.access.checkout.validation.validators.CvcValidator
 import com.worldpay.access.checkout.validation.validators.PanValidator
 import org.junit.Before
 import org.junit.Test
-import kotlin.test.assertEquals
 
 class PanTextWatcherTest {
 
-    private lateinit var cvcValidationRuleManager: CVCValidationRuleManager
-
+    private val cvcValidationRuleManager = mock<CVCValidationRuleManager>()
+    private val panValidator = mock<PanValidator>()
     private val panValidationResultHandler = mock<PanValidationResultHandler>()
     private val brandChangedHandler = mock<BrandChangedHandler>()
 
@@ -36,10 +35,8 @@ class PanTextWatcherTest {
     fun setup() {
         mockSuccessfulCardConfiguration()
 
-        cvcValidationRuleManager = CVCValidationRuleManager()
-
         panTextWatcher = PanTextWatcher(
-            panValidator = PanValidator(),
+            panValidator = panValidator,
             panValidationResultHandler = panValidationResultHandler,
             brandChangedHandler = brandChangedHandler,
             cvcEditText = cvcEditText,
@@ -52,8 +49,17 @@ class PanTextWatcherTest {
     }
 
     @Test
-    fun `should pass validation rule to validation rule handler where result is true for known brand`() {
+    fun `should call the pan validator with the correct validation rule`() {
         given(panEditable.toString()).willReturn(VISA_PAN)
+
+        panTextWatcher.afterTextChanged(panEditable)
+
+        verify(panValidator).validate(VISA_PAN, VISA_BRAND.pan)
+    }
+
+    @Test
+    fun `should call the pan result handler with valid result when the pan validator returns true`() {
+        mockPan(VISA_PAN, true)
 
         panTextWatcher.afterTextChanged(panEditable)
 
@@ -61,17 +67,8 @@ class PanTextWatcherTest {
     }
 
     @Test
-    fun `should pass validation rule to validation rule handler where result is true for unknown brand`() {
-        given(panEditable.toString()).willReturn(VALID_UNKNOWN_LUHN)
-
-        panTextWatcher.afterTextChanged(panEditable)
-
-        verify(panValidationResultHandler).handleResult(true)
-    }
-
-    @Test
-    fun `should pass validation rule to validation rule handler where result is false for unknown brand`() {
-        given(panEditable.toString()).willReturn("000000")
+    fun `should call the pan result handler with invalid result when the pan validator returns false`() {
+        mockPan(INVALID_UNKNOWN_LUHN, false)
 
         panTextWatcher.afterTextChanged(panEditable)
 
@@ -79,57 +76,88 @@ class PanTextWatcherTest {
     }
 
     @Test
-    fun `should pass validation rule to validation rule handler where result is false for known brand`() {
-        given(panEditable.toString()).willReturn("411")
+    fun `should call the brand changed handler with visa brand regardless of the pan validation result`() {
+        mockPan(VISA_PAN, true)
 
         panTextWatcher.afterTextChanged(panEditable)
+        verify(brandChangedHandler).handle(VISA_BRAND)
 
-        verify(panValidationResultHandler).handleResult(false)
+        mockPan(INVALID_UNKNOWN_LUHN, false)
+
+        panTextWatcher.afterTextChanged(panEditable)
+        verify(brandChangedHandler).handle(null)
     }
 
     @Test
-    fun `should revalidate the cvc when the brand changes and cvc is not empty`() {
-        given(panEditable.toString()).willReturn(VISA_PAN)
-        given(cvcEditable.toString()).willReturn("123")
-
-        assertEquals(CVC_DEFAULTS, cvcValidationRuleManager.getRule())
+    fun `should not call the brand changed handler when unknown pan is entered for the first time`() {
+        mockPan(VALID_UNKNOWN_LUHN, true)
 
         panTextWatcher.afterTextChanged(panEditable)
 
-        assertEquals(VISA_BRAND.cvc, cvcValidationRuleManager.getRule())
-
-        verify(cvcValidator).validate("123")
+        verifyZeroInteractions(brandChangedHandler)
     }
 
     @Test
-    fun `should handle brand change when brand is different from the last known brand`() {
-        given(panEditable.toString()).willReturn(VISA_PAN)
+    fun `should not call the brand changed handler if the brand has not actually changed from the previous one`() {
+        // set the visa pan so that the brand changed handler is called with visa
+        mockPan(VISA_PAN, true)
+        panTextWatcher.afterTextChanged(panEditable)
+        verify(brandChangedHandler).handle(VISA_BRAND)
+
+        reset(brandChangedHandler)
+
+        // set the visa pan again so that the brand changed handler is no longer called
+        mockPan(VISA_PAN, true)
+        panTextWatcher.afterTextChanged(panEditable)
+        verifyZeroInteractions(brandChangedHandler)
+    }
+
+    @Test
+    fun `should update the cvc validation rule when the brand changes`() {
+        mockPan(VISA_PAN, true)
 
         panTextWatcher.afterTextChanged(panEditable)
 
         verify(brandChangedHandler).handle(VISA_BRAND)
+        verify(cvcValidationRuleManager).updateRule(VISA_BRAND.cvc)
     }
 
     @Test
-    fun `should not revalidate the cvc if the cvc is blank and should only update the rule`() {
-        given(panEditable.toString()).willReturn(VISA_PAN)
+    fun `should re-validate the cvc when the brand changes`() {
+        mockPan(VISA_PAN, true)
+        given(cvcEditable.toString()).willReturn("123")
+
+        panTextWatcher.afterTextChanged(panEditable)
+
+        verify(brandChangedHandler).handle(VISA_BRAND)
+        verify(cvcValidator).validate("123")
+    }
+
+    @Test
+    fun `should not interact with the cvc validator at all if the brand has not changed`() {
+        // set the visa pan so that the brand changed handler is called with visa
+        mockPan(VISA_PAN, true)
+        panTextWatcher.afterTextChanged(panEditable)
+        verify(brandChangedHandler).handle(VISA_BRAND)
+
+        reset(brandChangedHandler)
+
+        // set the visa pan again so that the brand changed handler is no longer called
+        mockPan(VISA_PAN, true)
+        panTextWatcher.afterTextChanged(panEditable)
+        verifyZeroInteractions(brandChangedHandler)
+        verifyZeroInteractions(cvcValidator)
+    }
+
+    @Test
+    fun `should not interact with the cvc validator at all if the cvc is empty`() {
+        mockPan(VISA_PAN, true)
         given(cvcEditable.toString()).willReturn("")
 
-        assertEquals(CVC_DEFAULTS, cvcValidationRuleManager.getRule())
-
         panTextWatcher.afterTextChanged(panEditable)
 
-        assertEquals(VISA_BRAND.cvc, cvcValidationRuleManager.getRule())
-        verify(cvcValidator, never()).validate(any())
-    }
-
-    @Test
-    fun `should not revalidate the cvc when the brand doesn't change`() {
-        given(panEditable.toString()).willReturn("000000")
-
-        panTextWatcher.afterTextChanged(panEditable)
-
-        verify(cvcValidator, never()).validate(any())
+        verify(brandChangedHandler).handle(VISA_BRAND)
+        verifyZeroInteractions(cvcValidator)
     }
 
     @Test
@@ -145,13 +173,18 @@ class PanTextWatcherTest {
             cvcValidationRuleManager = cvcValidationRuleManager
         )
 
-        panTextWatcher.beforeTextChanged("", 1, 2,3)
-        panTextWatcher.onTextChanged("", 1, 2,3)
+        panTextWatcher.beforeTextChanged("", 1, 2, 3)
+        panTextWatcher.onTextChanged("", 1, 2, 3)
 
         verifyZeroInteractions(
             panValidator,
             panValidationResultHandler
         )
+    }
+
+    private fun mockPan(pan: String, isValid: Boolean) {
+        given(panEditable.toString()).willReturn(pan)
+        given(panValidator.validate(eq(pan), any())).willReturn(isValid)
     }
 
 }
