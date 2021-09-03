ae7f4ceeb9499ed1ac21bceebe6d533f4403a129
Mac Adamarczuk <mac.adamarczuk@fisglobal.com>
2021-09-03 15:07:34 +0100

    US632063: add test confirming baseUrl gets sanitised

--- access-checkout/src/main/java/com/worldpay/access/checkout/client/session/BaseUrlSanitiser.kt
+++ access-checkout/src/main/java/com/worldpay/access/checkout/client/session/BaseUrlSanitiser.kt
@@ -4,7 +4,7 @@ internal object BaseUrlSanitiser {
 
     fun sanitise(baseUrl: String?): String? {
         if (baseUrl == null || baseUrl.takeLast(1) != "/") {
-             return baseUrl
+            return baseUrl
         }
 
         return baseUrl.dropLast(1)
