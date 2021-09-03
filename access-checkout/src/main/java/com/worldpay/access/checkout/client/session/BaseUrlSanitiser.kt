754a7680393868c14b4497f632e9116438c90814
Mac Adamarczuk <mac.adamarczuk@fisglobal.com>
2021-09-03 10:18:58 +0100

    US632063: remove trailing slash from baseurl

---
+++ access-checkout/src/main/java/com/worldpay/access/checkout/client/session/BaseUrlSanitiser.kt
@@ -0,0 +1,12 @@
+package com.worldpay.access.checkout.client.session
+
+internal object BaseUrlSanitiser {
+
+    fun sanitise(baseUrl: String?): String? {
+        if (baseUrl == null || baseUrl.takeLast(1) != "/") {
+             return baseUrl
+        }
+
+        return baseUrl.dropLast(1)
+    }
+}
