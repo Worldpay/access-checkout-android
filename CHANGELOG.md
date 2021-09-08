e20a73c6dbb09dfb3d3f1f114b2a1ab75b77b089
Juned Miah <miahj13@gmail.com>
2021-09-08 13:32:28 +0100

    US637083: Added release script (#151)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,11 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/), and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+### Unreleased
+#### Added
+
+#### Changed
+
 ### [v2.2.0](https://github.com/Worldpay/access-checkout-android/releases/tag/v2.2.0) - 2021-09-07
 #### Added
 - Ability to enable Pan formatting by calling `enablePanFormatting()` when building `CardValidationConfig`
---
+++ scripts/release.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+
+NEW_VERSION=$1
+BRANCH_NAME="version-update-$NEW_VERSION"
+AAR_FILE=./access-checkout/build/outputs/aar/access-checkout-android-$NEW_VERSION.aar
+JAVADOC_FILE=./access-checkout/build/libs/access-checkout-android-$NEW_VERSION-javadoc.jar
+SOURCES_FILE=./access-checkout/build/libs/access-checkout-android-$NEW_VERSION-sources.jar
+CURRENT_DATE=$(date +"%Y-%m-%d")
+
+git checkout master
+git pull
+
+git branch -d "$BRANCH_NAME" 2> /dev/null
+git push origin --delete "$BRANCH_NAME" 2> /dev/null
+
+git checkout -b "$BRANCH_NAME"
+
+sed -i -e "s/version=.*/version=$NEW_VERSION/g" ./access-checkout/gradle.properties
+rm -f ./access-checkout/gradle.properties-e
+
+sed -i -e "s/### Unreleased/### [v$NEW_VERSION](https:\/\/github.com\/Worldpay\/access-checkout-android\/releases\/tag\/v$NEW_VERSION) - $CURRENT_DATE/g" ./CHANGELOG.md
+rm -f CHANGELOG.md-e
+
+awk 'NR==5{print "\n### Unreleased\n#### Added\n\n#### Changed"}1' ./CHANGELOG.md > ./CHANGELOG.new
+rm -f CHANGELOG.md && mv ./CHANGELOG.new ./CHANGELOG.md
+
+echo "Verifying release artifacts"
+./gradlew clean :access-checkout:assembleRelease javadocJar sourcesJar
+
+if [ -f "$AAR_FILE" ]; then
+    echo "$AAR_FILE exists."
+else
+    echo "$AAR_FILE does not exist."
+    exit 1
+fi
+
+if [ -f "$JAVADOC_FILE" ]; then
+    echo "$JAVADOC_FILE exists."
+else
+    echo "$JAVADOC_FILE does not exist."
+    exit 1
+fi
+
+if [ -f "$SOURCES_FILE" ]; then
+    echo "$SOURCES_FILE exists."
+else
+    echo "$SOURCES_FILE does not exist."
+    exit 1
+fi
+
+echo "Pushing to git branch - origin/$BRANCH_NAME"
+git add ./access-checkout/gradle.properties
+git add ./CHANGELOG.md
+git commit -m "Updated Android SDK version to $NEW_VERSION"
+git push --set-upstream origin "$BRANCH_NAME"
+
+echo "*******************************************************************"
+echo "Version updated to $NEW_VERSION"
+echo "Please ensure that the CHANGELOG.md has been updated accordingly"
+echo "*******************************************************************"
