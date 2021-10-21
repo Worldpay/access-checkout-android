01ceaea883ca4dbad16c0a6b3d0bb694a5e4a412
Vishal Jethwa <vishal.jethwa@fisglobal.com>
2021-10-21 15:54:59 +0100

    add variable to set detect version

--- scripts/blackduck/run_scan.sh
+++ scripts/blackduck/run_scan.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+echo "Setting version of Detect"
+export DETECT_LATEST_RELEASE_VERSION=$blackduck_detect_version
 
 echo "Initiating Blackduck Scan..."
 
 curl -LOk https://detect.synopsys.com/detect.sh
 chmod +x ./detect.sh
 SDK_VERSION=$(grep 'version=' access-checkout/gradle.properties | cut -d= -f2)
-./detect.sh --blackduck.url="https://fis2.app.blackduck.com/" --blackduck.api.token=$hydra_aco_blackduck_token --blackduck.trust.cert=true --detect.project.name=$BLACKDUCK_PROJECT_NAME --detect.project.version.name=$SDK_VERSION --detect.risk.report.pdf=true
+./detect.sh --blackduck.url="https://fis2.app.blackduck.com/" --blackduck.api.token=$hydra_aco_blackduck_token --blackduck.trust.cert=true --detect.project.name=$BLACKDUCK_PROJECT_NAME --detect.project.version.name=$SDK_VERSION --detect.risk.report.pdf=true --detect.cleanup=false
