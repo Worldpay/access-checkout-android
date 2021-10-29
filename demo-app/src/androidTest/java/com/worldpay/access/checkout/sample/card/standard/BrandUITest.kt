ff99e9d0eb04f540c163a0f3a4d53a37f95ebac5
Olivier Chalet <olivier.chalet@worldpay.com>
2021-10-29 16:53:30 +0100

    US710768: refactor Card Brands UI tests to cover more BIN ranges

--- demo-app/src/androidTest/java/com/worldpay/access/checkout/sample/card/standard/BrandUITest.kt
+++ demo-app/src/androidTest/java/com/worldpay/access/checkout/sample/card/standard/BrandUITest.kt
@@ -36,48 +36,6 @@ class BrandUITest : AbstractCardFragmentTest() {
     }
 
     @Test
-    fun shouldDisplayAmexBrandImage() = checkBrandImageFor(AMEX_PAN, AMEX)
-
-    @Test
-    fun shouldDisplayVisaBrandImage() = checkBrandImageFor(VISA_PAN, VISA)
-
-    @Test
-    fun shouldDisplayMastercardBrandImage() = checkBrandImageFor(MASTERCARD_PAN, MASTERCARD)
-
-    @Test
-    fun shouldDisplayDinersBrandImage() = checkBrandImageFor(DINERS_PAN, DINERS)
-
-    @Test
-    fun shouldDisplayDiscoverBrandImage() = checkBrandImageFor(DISCOVER_PAN, DISCOVER)
-
-    @Test
-    fun shouldDisplayJCBBrandImage() = checkBrandImageFor(JCB_PAN, JCB)
-
-    @Test
-    fun shouldDisplayMaestroBrandImage() = checkBrandImageFor(MAESTRO_PAN, MAESTRO)
-
-    @Test
-    fun shouldDisplayAmexBrandImageForPartialPan() = checkBrandImageFor(asPartial(AMEX_PAN), AMEX)
-
-    @Test
-    fun shouldDisplayVisaBrandImageForPartialPan() = checkBrandImageFor(asPartial(VISA_PAN), VISA)
-
-    @Test
-    fun shouldDisplayMastercardBrandImageForPartialPan() = checkBrandImageFor(asPartial(MASTERCARD_PAN), MASTERCARD)
-
-    @Test
-    fun shouldDisplayDinersBrandImageForPartialPan() = checkBrandImageFor(asPartial(DINERS_PAN), DINERS)
-
-    @Test
-    fun shouldDisplayDiscoverBrandImageForPartialPan() = checkBrandImageFor(asPartial(DISCOVER_PAN), DISCOVER)
-
-    @Test
-    fun shouldDisplayJCBBrandImageForPartialPan() = checkBrandImageFor(asPartial(JCB_PAN), JCB)
-
-    @Test
-    fun shouldDisplayMaestroBrandImageForPartialPan() = checkBrandImageFor(asPartial(MAESTRO_PAN), MAESTRO)
-
-    @Test
     fun shouldChangeBrandImageWhenPanChanges() {
         cardFragmentTestUtils
             .isInInitialState()
@@ -88,11 +46,201 @@ class BrandUITest : AbstractCardFragmentTest() {
             .hasBrand(JCB)
     }
 
-    private fun checkBrandImageFor(pan: String, brand: CardBrand) {
+    // AMEX
+    @Test
+    fun shouldDisplayBrandImage_AMEX_ForCompletePan() = checkBrandImageFor(AMEX_PAN, isBrand = AMEX)
+
+    @Test
+    fun shouldDisplayBrandImage_AMEX_ForRange_34() {
+        checkBrandImageFor("3400", isBrand = AMEX)
+        checkBrandImageFor("3499", isBrand = AMEX)
+    }
+
+    @Test
+    fun shouldDisplayBrandImage_AMEX_ForRange_37() {
+        checkBrandImageFor("3700", isBrand = AMEX)
+        checkBrandImageFor("3799", isBrand = AMEX)
+    }
+
+    // DINERS
+    @Test
+    fun shouldDisplayBrandImage_DINERS_ForCompletePan() = checkBrandImageFor(DINERS_PAN, isBrand = DINERS)
+
+    @Test
+    fun shouldDisplayBrandImage_DINERS_ForRange_300_305() {
+        checkBrandImageFor("3000", isBrand = DINERS)
+        checkBrandImageFor("3059", isBrand = DINERS)
+    }
+
+    @Test
+    fun shouldDisplayBrandImage_DINERS_ForRange_36() {
+        checkBrandImageFor("360", isBrand = DINERS)
+        checkBrandImageFor("369", isBrand = DINERS)
+    }
+
+    @Test
+    fun shouldDisplayBrandImage_DINERS_ForRange_38() {
+        checkBrandImageFor("380", isBrand = DINERS)
+        checkBrandImageFor("389", isBrand = DINERS)
+    }
+
+    @Test
+    fun shouldDisplayBrandImage_DINERS_ForRange_39() {
+        checkBrandImageFor("390", isBrand = DINERS)
+        checkBrandImageFor("399", isBrand = DINERS)
+    }
+
+    // DISCOVER
+    @Test
+    fun shouldDisplayBrandImage_DISCOVER_ForCompletePan() = checkBrandImageFor(DISCOVER_PAN, isBrand = DISCOVER)
+
+    @Test
+    fun shouldDisplayBrandImage_DISCOVER_ForRange_6011() {
+        checkBrandImageFor("60110", isBrand = DISCOVER)
+        checkBrandImageFor("60119", isBrand = DISCOVER)
+    }
+
+    @Test
+    fun shouldDisplayBrandImage_DISCOVER_ForRange_644_649() {
+        checkBrandImageFor("6440", isBrand = DISCOVER)
+        checkBrandImageFor("6449", isBrand = DISCOVER)
+    }
+
+    @Test
+    fun shouldDisplayBrandImage_DISCOVER_ForRange_65() {
+        checkBrandImageFor("650", isBrand = DISCOVER)
+        checkBrandImageFor("659", isBrand = DISCOVER)
+    }
+
+    // JCB
+    @Test
+    fun shouldDisplayBrandImage_JCB_ForCompletePan() = checkBrandImageFor(JCB_PAN, isBrand = JCB)
+
+    @Test
+    fun shouldDisplayBrandImage_JCB_ForRange_352_358() {
+        checkBrandImageFor("3520", isBrand = JCB)
+        checkBrandImageFor("3589", isBrand = JCB)
+    }
+
+    @Test
+    fun shouldDisplayBrandImage_JCB_ForRange_2131() {
+        checkBrandImageFor("21310", isBrand = JCB)
+        checkBrandImageFor("21319", isBrand = JCB)
+    }
+
+    @Test
+    fun shouldDisplayBrandImage_JCB_ForRange_1800() {
+        checkBrandImageFor("18000", isBrand = JCB)
+        checkBrandImageFor("18009", isBrand = JCB)
+    }
+
+    // MAESTRO
+    @Test
+    fun shouldDisplayBrandImage_MAESTRO_ForCompletePan() = checkBrandImageFor(MAESTRO_PAN, isBrand = MAESTRO)
+
+    @Test
+    fun shouldDisplayBrandImage_MAESTRO_ForRange_493698() {
+        checkBrandImageFor("4936980", isBrand = MAESTRO)
+        checkBrandImageFor("4936989", isBrand = MAESTRO)
+    }
+
+    @Test
+    fun shouldDisplayBrandImage_MAESTRO_ForRange_50000_50599() {
+        checkBrandImageFor("500000", isBrand = MAESTRO)
+        checkBrandImageFor("505999", isBrand = MAESTRO)
+    }
+
+    @Test
+    fun shouldDisplayBrandImage_MAESTRO_ForRange_5060_5065() {
+        checkBrandImageFor("50600", isBrand = MAESTRO)
+        checkBrandImageFor("50659", isBrand = MAESTRO)
+    }
+
+    @Test
+    fun shouldDisplayBrandImage_MAESTRO_ForRange_5066() {
+        checkBrandImageFor("50660", isBrand = MAESTRO)
+        checkBrandImageFor("50669", isBrand = MAESTRO)
+    }
+
+    @Test
+    fun shouldDisplayBrandImage_MAESTRO_ForRange_50677_50679() {
+        checkBrandImageFor("506770", isBrand = MAESTRO)
+        checkBrandImageFor("506799", isBrand = MAESTRO)
+    }
+
+    @Test
+    fun shouldDisplayBrandImage_MAESTRO_ForRange_50680_50699() {
+        checkBrandImageFor("506800", isBrand = MAESTRO)
+        checkBrandImageFor("506999", isBrand = MAESTRO)
+    }
+
+    @Test
+    fun shouldDisplayBrandImage_MAESTRO_ForRange_50700_50899() {
+        checkBrandImageFor("507000", isBrand = MAESTRO)
+        checkBrandImageFor("507999", isBrand = MAESTRO)
+    }
+
+    @Test
+    fun shouldDisplayBrandImage_MAESTRO_ForRange_56_59() {
+        checkBrandImageFor("560", isBrand = MAESTRO)
+        checkBrandImageFor("599", isBrand = MAESTRO)
+    }
+
+    @Test
+    fun shouldDisplayBrandImage_MAESTRO_ForRange_63() {
+        checkBrandImageFor("630", isBrand = MAESTRO)
+        checkBrandImageFor("639", isBrand = MAESTRO)
+    }
+
+    @Test
+    fun shouldDisplayBrandImage_MAESTRO_ForRange_67() {
+        checkBrandImageFor("670", isBrand = MAESTRO)
+        checkBrandImageFor("679", isBrand = MAESTRO)
+    }
+
+    // MASTERCARD
+    @Test
+    fun shouldDisplayBrandImage_MASTERCARD_ForCompletePan() = checkBrandImageFor(MASTERCARD_PAN, isBrand = MASTERCARD)
+
+    @Test
+    fun shouldDisplayBrandImage_MASTERCARD_ForRange_51_55() {
+        checkBrandImageFor("510", isBrand = MASTERCARD)
+        checkBrandImageFor("559", isBrand = MASTERCARD)
+    }
+
+    @Test
+    fun shouldDisplayBrandImage_MASTERCARD_ForRange_22_27() {
+        checkBrandImageFor("220", isBrand = MASTERCARD)
+        checkBrandImageFor("279", isBrand = MASTERCARD)
+    }
+
+    // VISA
+    @Test
+    fun shouldDisplayBrandImage_VISA_ForCompletePan() = checkBrandImageFor(VISA_PAN, isBrand = VISA)
+
+    @Test
+    fun shouldDisplayBrandImage_VISA_ForRange4() {
+        checkBrandImageFor("40", isBrand = VISA)
+        checkBrandImageFor("49", isBrand = VISA)
+    }
+
+    // This test covers an edge case of a partial pan that looks like a visa pan
+    @Test
+    fun shouldDisplayBrandImage_MAESTRO_For493698() {
+        cardFragmentTestUtils
+            .isInInitialState()
+            .hasNoBrand()
+            .enterCardDetails(pan = "493698")
+            .hasBrand(MAESTRO)
+    }
+
+    private fun checkBrandImageFor(pan: String, isBrand: CardBrand) {
+        clearPan()
+
         cardFragmentTestUtils
             .isInInitialState()
             .hasNoBrand()
             .enterCardDetails(pan = pan)
-            .hasBrand(brand)
+            .hasBrand(isBrand)
     }
 }
