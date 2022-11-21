bc0a2e7069197032e0d199e2fd598dbd1ba4ad07
Vishal Jethwa <vishal.jethwa@fisglobal.com>
2022-11-21 13:48:06 +0000

    US1284253: updated exclude project path property

--- scripts/blackduck/run_scan.sh
+++ scripts/blackduck/run_scan.sh
@@ -8,7 +8,7 @@ chmod +x ./detect.sh
 SDK_VERSION=$(grep 'version=' access-checkout/gradle.properties | cut -d= -f2)
 if [ $IS_RELEASE_SCAN -eq 0 ]
 then
-  ./detect.sh --blackduck.url="https://fis2.app.blackduck.com/" --blackduck.api.token=$hydra_aco_blackduck_token --blackduck.trust.cert=true --detect.project.name=$BLACKDUCK_PROJECT_NAME --detect.project.version.name=$SDK_VERSION --detect.risk.report.pdf=true --detect.gradle.excluded.project.paths=demo-app --detect.gradle.excluded.configurations=androidTestImplementation,testImplementation,dokkaSourceSets,ktlint
+  ./detect.sh --blackduck.url="https://fis2.app.blackduck.com/" --blackduck.api.token=$hydra_aco_blackduck_token --blackduck.trust.cert=true --detect.project.name=$BLACKDUCK_PROJECT_NAME --detect.project.version.name=$SDK_VERSION --detect.risk.report.pdf=true --detect.gradle.excluded.project.paths=:access-checkout:demo-app --detect.gradle.excluded.configurations=androidTestImplementation,testImplementation,dokkaSourceSets,ktlint
 else
   echo "Executing Release Scan with name $SDK_VERSION-RELEASE"
   ./detect.sh --blackduck.url="https://fis2.app.blackduck.com/" --blackduck.api.token=$hydra_aco_blackduck_token --blackduck.trust.cert=true --detect.project.name=$BLACKDUCK_PROJECT_NAME --detect.project.version.name="$SDK_VERSION-RELEASE" --detect.risk.report.pdf=true --detect.gradle.excluded.configurations=androidTestImplementation,testImplementation
