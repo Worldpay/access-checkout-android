17e1a3fb53fa74ced8b29dbabb3ec8b7bc02217e
Olivier Chalet <olivier.chalet@fisglobal.com>
2023-04-06 11:22:12 +0100

    US1522531: change Blackduck pipeline to use detect8

--- scripts/blackduck/run_scan.sh
+++ scripts/blackduck/run_scan.sh
@@ -3,16 +3,17 @@ echo "Setting version of Detect"
 export DETECT_LATEST_RELEASE_VERSION=$BLACKDUCK_DETECT_VERSION
 
 echo "Initiating Blackduck Scan..."
-curl -LOk https://detect.synopsys.com/detect.sh
+curl -LOk https://detect.synopsys.com/detect8.sh
+mv  detect8.sh ./detect.sh
 chmod +x ./detect.sh
 
 SDK_VERSION=$(grep 'version=' access-checkout/gradle.properties | cut -d= -f2)
 GRADLE_CONFIGURATIONS_TO_SCAN="releaseCompileClasspath,releaseRuntimeClasspath"
 
-if [ $IS_RELEASE_SCAN -eq 0 ]
+if [ $IS_RELEASE_SCAN -eq 1 ]
 then
-  ./detect.sh --blackduck.url="https://fis2.app.blackduck.com/" --blackduck.api.token=$hydra_aco_blackduck_token --blackduck.trust.cert=true --detect.project.name=$BLACKDUCK_PROJECT_NAME --detect.project.version.name=$SDK_VERSION --detect.risk.report.pdf=true --detect.gradle.included.configurations="$GRADLE_CONFIGURATIONS_TO_SCAN"
-else
-  echo "Executing Release Scan with name $SDK_VERSION-RELEASE"
-  ./detect.sh --blackduck.url="https://fis2.app.blackduck.com/" --blackduck.api.token=$hydra_aco_blackduck_token --blackduck.trust.cert=true --detect.project.name=$BLACKDUCK_PROJECT_NAME --detect.project.version.name="$SDK_VERSION-RELEASE" --detect.risk.report.pdf=true --detect.gradle.included.configurations="$GRADLE_CONFIGURATIONS_TO_SCAN"
+  echo "Executing Release Scan with name [version]-RELEASE"
+  SDK_VERSION="${SDK_VERSION}-RELEASE"
 fi
+
+./detect.sh --blackduck.url="https://fis2.app.blackduck.com/" --blackduck.api.token=$hydra_aco_blackduck_token --blackduck.trust.cert=true --detect.project.name=$BLACKDUCK_PROJECT_NAME --detect.project.version.name=$SDK_VERSION --detect.risk.report.pdf=true --detect.gradle.included.configurations="$GRADLE_CONFIGURATIONS_TO_SCAN"
