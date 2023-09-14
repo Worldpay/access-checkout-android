fd5b6c61fa3024bb27d0e39596d7c08182de9469
Jason Dzela-Mensah <jason.dzelamensah@fisglobal.com>
2023-09-14 13:51:32 +0100

    US1698897: add required methods in AccessEditText to fix failing unit tests; refactor ExpiryFormattingIntegrationTest to remove .toString() calls

--- access-checkout/src/test/java/com/worldpay/access/checkout/client/validation/expiry/ExpiryFormattingIntegrationTest.kt
+++ access-checkout/src/test/java/com/worldpay/access/checkout/client/validation/expiry/ExpiryFormattingIntegrationTest.kt
@@ -2,10 +2,10 @@ package com.worldpay.access.checkout.client.validation.expiry
 
 import com.worldpay.access.checkout.client.testutil.AbstractValidationIntegrationTest
 import com.worldpay.access.checkout.testutils.PlainRobolectricTestRunner
-import kotlin.test.assertEquals
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import kotlin.test.assertEquals
 
 @RunWith(PlainRobolectricTestRunner::class)
 class ExpiryFormattingIntegrationTest : AbstractValidationIntegrationTest() {
@@ -18,115 +18,115 @@ class ExpiryFormattingIntegrationTest : AbstractValidationIntegrationTest() {
     @Test
     fun `should append forward slash after month is entered`() {
         expiryDate.setText("02")
-        assertEquals("02/", expiryDate.text.toString())
+        assertEquals("02/", expiryDate.text)
     }
 
     @Test
     fun `should be able to edit month independently without reformatting`() {
         expiryDate.setText("01/29")
-        assertEquals("01/29", expiryDate.text.toString())
+        assertEquals("01/29", expiryDate.text)
 
         expiryDate.setText("0/29")
-        assertEquals("0/29", expiryDate.text.toString())
+        assertEquals("0/29", expiryDate.text)
 
         expiryDate.setText("/29")
-        assertEquals("/29", expiryDate.text.toString())
+        assertEquals("/29", expiryDate.text)
 
         expiryDate.setText("1/29")
-        assertEquals("1/29", expiryDate.text.toString())
+        assertEquals("1/29", expiryDate.text)
     }
 
     @Test
     fun `should reformat pasted new date overwriting an existing one`() {
         expiryDate.setText("01/19")
-        assertEquals("01/19", expiryDate.text.toString())
+        assertEquals("01/19", expiryDate.text)
 
         expiryDate.setText("1299")
-        assertEquals("12/99", expiryDate.text.toString())
+        assertEquals("12/99", expiryDate.text)
 
         expiryDate.setText("1298")
-        assertEquals("12/98", expiryDate.text.toString())
+        assertEquals("12/98", expiryDate.text)
 
         expiryDate.setText("12/98")
-        assertEquals("12/98", expiryDate.text.toString())
+        assertEquals("12/98", expiryDate.text)
 
         expiryDate.setText("12")
-        assertEquals("12/", expiryDate.text.toString())
+        assertEquals("12/", expiryDate.text)
 
         expiryDate.setText("12")
-        assertEquals("12", expiryDate.text.toString())
+        assertEquals("12", expiryDate.text)
     }
 
     @Test
     fun `should be able to delete characters to empty from valid expiry date`() {
         expiryDate.setText("12/99")
-        assertEquals("12/99", expiryDate.text.toString())
+        assertEquals("12/99", expiryDate.text)
 
         expiryDate.setText("12/9")
-        assertEquals("12/9", expiryDate.text.toString())
+        assertEquals("12/9", expiryDate.text)
 
         expiryDate.setText("12/")
-        assertEquals("12/", expiryDate.text.toString())
+        assertEquals("12/", expiryDate.text)
 
         expiryDate.setText("12")
-        assertEquals("12", expiryDate.text.toString())
+        assertEquals("12", expiryDate.text)
 
         expiryDate.setText("1")
-        assertEquals("1", expiryDate.text.toString())
+        assertEquals("1", expiryDate.text)
 
         expiryDate.setText("")
-        assertEquals("", expiryDate.text.toString())
+        assertEquals("", expiryDate.text)
     }
 
     @Test
     fun `should be able to delete characters to empty from invalid expiry date`() {
         expiryDate.setText("13/99")
-        assertEquals("13/99", expiryDate.text.toString())
+        assertEquals("13/99", expiryDate.text)
 
         expiryDate.setText("13/9")
-        assertEquals("13/9", expiryDate.text.toString())
+        assertEquals("13/9", expiryDate.text)
 
         expiryDate.setText("13/")
-        assertEquals("13/", expiryDate.text.toString())
+        assertEquals("13/", expiryDate.text)
 
         expiryDate.setText("13")
-        assertEquals("13", expiryDate.text.toString())
+        assertEquals("13", expiryDate.text)
 
         expiryDate.setText("1")
-        assertEquals("1", expiryDate.text.toString())
+        assertEquals("1", expiryDate.text)
 
         expiryDate.setText("")
-        assertEquals("", expiryDate.text.toString())
+        assertEquals("", expiryDate.text)
     }
 
     @Test
     fun `should not reformat pasted value when pasted value is same as current value`() {
         expiryDate.setText("12/")
-        assertEquals("12/", expiryDate.text.toString())
+        assertEquals("12/", expiryDate.text)
 
         expiryDate.setText("12")
-        assertEquals("12", expiryDate.text.toString())
+        assertEquals("12", expiryDate.text)
     }
 
     @Test
     fun `should be able to add characters to complete`() {
         expiryDate.setText("")
-        assertEquals("", expiryDate.text.toString())
+        assertEquals("", expiryDate.text)
 
         expiryDate.setText("1")
-        assertEquals("1", expiryDate.text.toString())
+        assertEquals("1", expiryDate.text)
 
         expiryDate.setText("12")
-        assertEquals("12/", expiryDate.text.toString())
+        assertEquals("12/", expiryDate.text)
 
         expiryDate.setText("12/")
-        assertEquals("12/", expiryDate.text.toString())
+        assertEquals("12/", expiryDate.text)
 
         expiryDate.setText("12/9")
-        assertEquals("12/9", expiryDate.text.toString())
+        assertEquals("12/9", expiryDate.text)
 
         expiryDate.setText("12/99")
-        assertEquals("12/99", expiryDate.text.toString())
+        assertEquals("12/99", expiryDate.text)
     }
 
     @Test
@@ -171,10 +171,10 @@ class ExpiryFormattingIntegrationTest : AbstractValidationIntegrationTest() {
     @Test
     fun `should reformat when month value changes despite the separator being deleted`() {
         expiryDate.setText("02/")
-        assertEquals("02/", expiryDate.text.toString())
+        assertEquals("02/", expiryDate.text)
 
         expiryDate.setText("03")
-        assertEquals("03/", expiryDate.text.toString())
+        assertEquals("03/", expiryDate.text)
     }
 
     @Test
@@ -213,6 +213,6 @@ class ExpiryFormattingIntegrationTest : AbstractValidationIntegrationTest() {
 
     private fun enterAndGetText(string: String): String {
         expiryDate.setText(string)
-        return expiryDate.text.toString()
+        return expiryDate.text
     }
 }
