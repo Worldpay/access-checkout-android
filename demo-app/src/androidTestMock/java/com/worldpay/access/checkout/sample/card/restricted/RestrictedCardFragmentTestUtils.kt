ed03003828225cf52c7ec4cc5d5550a33f2975b7
Jason Dzela-Mensah <jason.dzelamensah@fisglobal.com>
2023-09-26 16:15:14 +0100

    US1698909: add new test util method to retrieve card details for demo app tests

--- demo-app/src/androidTestMock/java/com/worldpay/access/checkout/sample/card/restricted/RestrictedCardFragmentTestUtils.kt
+++ demo-app/src/androidTestMock/java/com/worldpay/access/checkout/sample/card/restricted/RestrictedCardFragmentTestUtils.kt
@@ -1,6 +1,5 @@
 package com.worldpay.access.checkout.sample.card.restricted
 
-import android.widget.EditText
 import android.widget.ImageView
 import androidx.core.view.isVisible
 import androidx.test.rule.ActivityTestRule
@@ -8,12 +7,15 @@ import com.worldpay.access.checkout.sample.MainActivity
 import com.worldpay.access.checkout.sample.R
 import com.worldpay.access.checkout.sample.card.standard.testutil.CardBrand
 import com.worldpay.access.checkout.sample.testutil.AbstractFragmentTestUtils
+import com.worldpay.access.checkout.sample.testutil.UITestUtils.retrieveEnteredText
+import com.worldpay.access.checkout.ui.AccessEditText
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
-class RestrictedCardFragmentTestUtils(activityRule: ActivityTestRule<MainActivity>) : AbstractFragmentTestUtils(activityRule) {
+class RestrictedCardFragmentTestUtils(activityRule: ActivityTestRule<MainActivity>) :
+    AbstractFragmentTestUtils(activityRule) {
 
-    private fun panInput() = findById<EditText>(R.id.restricted_card_flow_text_pan)
+    private fun panInput() = findById<AccessEditText>(R.id.restricted_card_flow_text_pan)
     private fun brandLogo() = findById<ImageView>(R.id.restricted_card_flow_brand_logo)
 
     fun isInInitialState(): RestrictedCardFragmentTestUtils {
@@ -42,7 +44,7 @@ class RestrictedCardFragmentTestUtils(activityRule: ActivityTestRule<MainActivit
     }
 
     fun cardDetailsAre(pan: String? = null): RestrictedCardFragmentTestUtils {
-        if (pan != null) wait { assertEquals(pan, panInput().text.toString()) }
+        if (pan != null) wait { assertEquals(pan, retrieveEnteredText(panInput())) }
         return this
     }
 
