fadb55893bd63e84e310b396ad4df1e5eafff7dd
Jason Dzela-Mensah <jason.dzelamensah@fisglobal.com>
2023-10-10 15:09:29 +0100

    US1698909: add tests for AttributeValues

---
+++ access-checkout/src/test/java/com/worldpay/access/checkout/ui/AttributeValuesTest.kt
@@ -0,0 +1,63 @@
+package com.worldpay.access.checkout.ui
+
+import android.content.Context
+import android.content.res.Resources
+import android.content.res.Resources.NotFoundException
+import android.util.AttributeSet
+import org.junit.Before
+import org.junit.Test
+import org.mockito.kotlin.any
+import org.mockito.kotlin.eq
+import org.mockito.kotlin.given
+import org.mockito.kotlin.mock
+import kotlin.test.assertEquals
+import kotlin.test.assertNull
+
+class AttributeValuesTest {
+    private lateinit var attributeValues: AttributeValues
+    private val contextMock: Context = mock()
+    private val attributeSetMock: AttributeSet = mock()
+    private val resourcesMock: Resources = mock()
+
+    @Before
+    fun setUp() {
+        attributeValues = AttributeValues(contextMock, attributeSetMock)
+    }
+
+    @Test
+    fun `should return string from attribute name`() {
+        val resId = 123
+        val attributeName = "some-name"
+        val expectedString = "some-hint"
+
+        given(attributeSetMock.getAttributeResourceValue(any(), eq(attributeName), any())).willReturn(resId)
+        given(resourcesMock.getString(resId)).willReturn(expectedString)
+        given(contextMock.resources).willReturn(resourcesMock)
+
+        assertEquals(expectedString, attributeValues.stringOf(attributeName))
+    }
+
+    @Test
+    fun `should return null when unable to find the resource`() {
+        val resId = 123
+        val attributeName = "some-name"
+
+        given(attributeSetMock.getAttributeResourceValue(any(), eq(attributeName), any())).willReturn(resId)
+        given(resourcesMock.getString(resId)).willThrow(NotFoundException())
+        given(contextMock.resources).willReturn(resourcesMock)
+
+        assertNull(attributeValues.stringOf(attributeName))
+    }
+
+    @Test
+    fun `should return string from attribute name when resource id is 0`() {
+        val attributeName = "some-name"
+        val expectedString = "some-hint"
+
+        given(attributeSetMock.getAttributeResourceValue(any(), eq(attributeName), any())).willReturn(0)
+        given(attributeSetMock.getAttributeValue(any(), eq(attributeName))).willReturn(expectedString)
+        given(contextMock.resources).willReturn(resourcesMock)
+
+        assertEquals(expectedString, attributeValues.stringOf(attributeName))
+    }
+}
