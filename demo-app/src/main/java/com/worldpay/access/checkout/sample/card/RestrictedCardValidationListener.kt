8137aa82e94657d75f67d933bee96b45617764dd
Michael McFeat <michael.mcfeat@worldpay.com>
2025-05-07 12:36:02 +0100

    US2121247: Pluralises card brand methods and updates merchant validation listener to handle card scheme changes

--- demo-app/src/main/java/com/worldpay/access/checkout/sample/card/RestrictedCardValidationListener.kt
+++ demo-app/src/main/java/com/worldpay/access/checkout/sample/card/RestrictedCardValidationListener.kt
@@ -1,5 +1,6 @@
 package com.worldpay.access.checkout.sample.card
 
+import android.util.Log
 import android.widget.ImageView
 import androidx.core.content.res.ResourcesCompat.getColor
 import androidx.fragment.app.FragmentActivity
@@ -23,9 +24,17 @@ class RestrictedCardValidationListener(private val activity: FragmentActivity) :
         changeFont(pan, isValid)
     }
 
-    override fun onBrandChange(cardBrand: CardBrand?) {
+    override fun onBrandsChange(cardBrands: List<CardBrand?>) {
         val brandLogo = activity.findViewById<ImageView>(R.id.restricted_card_flow_brand_logo)
-        getInstance(activity).fetchAndApplyCardLogo(cardBrand, brandLogo)
+        if (brandLogo != null) {
+            // currently just applying first card logo returned in list
+            // as underlying functionality to display two logos doesn't exist yet
+            // will be displayed in demo application as csv values below card number field
+            val cardBrand = cardBrands.firstOrNull()
+            getInstance(activity).fetchAndApplyCardLogo(cardBrand, brandLogo)
+        } else {
+            Log.d(this::class.java.simpleName, "Received CardBrand change but could not find ImageView with id `R.id.card_flow_brand_logo`")
+        }
     }
 
     override fun onExpiryDateValidated(isValid: Boolean) {
