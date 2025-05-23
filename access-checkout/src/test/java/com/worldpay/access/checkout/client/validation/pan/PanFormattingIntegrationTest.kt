15124ed15f18f3b10089227bfdbc85a1bf370d67
jmensahWP <jason.dzelamensah@worldpay.com>
2025-05-23 11:51:15 +0100

    US2121249: fix failing test

--- access-checkout/src/test/java/com/worldpay/access/checkout/client/validation/pan/PanFormattingIntegrationTest.kt
+++ access-checkout/src/test/java/com/worldpay/access/checkout/client/validation/pan/PanFormattingIntegrationTest.kt
@@ -5,6 +5,7 @@ import com.worldpay.access.checkout.client.testutil.AbstractValidationIntegratio
 import com.worldpay.access.checkout.testutils.CardConfigurationUtil.Brands.AMEX_BRAND
 import com.worldpay.access.checkout.testutils.CardConfigurationUtil.Brands.VISA_BRAND
 import com.worldpay.access.checkout.testutils.CardConfigurationUtil.toCardBrandList
+import com.worldpay.access.checkout.testutils.CardConfigurationUtil.toCardBrandListHardcoded
 import com.worldpay.access.checkout.testutils.CardNumberUtil.AMEX_PAN
 import com.worldpay.access.checkout.testutils.CardNumberUtil.AMEX_PAN_FORMATTED
 import com.worldpay.access.checkout.testutils.CardNumberUtil.visaPan
@@ -34,7 +35,7 @@ class PanFormattingIntegrationTest : AbstractValidationIntegrationTest() {
         shadowOf(getMainLooper()).waitForQueueUntilIdle()
 
         assertEquals("4111 1111 1111 1111", pan.text)
-        verify(cardValidationListener).onBrandsChange(toCardBrandList(VISA_BRAND))
+        verify(cardValidationListener).onBrandsChange(toCardBrandListHardcoded(VISA_BRAND))
         verify(cardValidationListener).onPanValidated(true)
     }
 
@@ -45,7 +46,7 @@ class PanFormattingIntegrationTest : AbstractValidationIntegrationTest() {
         pan.setText("342793178931249")
 
         assertEquals("3427 931789 31249", pan.text)
-        verify(cardValidationListener).onBrandsChange(toCardBrandList(AMEX_BRAND))
+        verify(cardValidationListener).onBrandsChange(toCardBrandListHardcoded(AMEX_BRAND))
         verify(cardValidationListener).onPanValidated(true)
     }
 
@@ -58,7 +59,7 @@ class PanFormattingIntegrationTest : AbstractValidationIntegrationTest() {
         shadowOf(getMainLooper()).waitForQueueUntilIdle()
 
         assertEquals("4111111111111111", pan.text)
-        verify(cardValidationListener).onBrandsChange(toCardBrandList(VISA_BRAND))
+        verify(cardValidationListener).onBrandsChange(toCardBrandListHardcoded(VISA_BRAND))
         verify(cardValidationListener).onPanValidated(true)
     }
 
