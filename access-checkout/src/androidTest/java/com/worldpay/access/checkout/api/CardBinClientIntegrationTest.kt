c12643d24d639c9c358d3e9415e9fb90d978445a
jmensahWP <jason.dzelamensah@worldpay.com>
2025-05-29 11:20:37 +0100

    US2126253: create unit test for card bin client and related classes; add exception handling to client

--- access-checkout/src/androidTest/java/com/worldpay/access/checkout/api/CardBinClientIntegrationTest.kt
+++ access-checkout/src/androidTest/java/com/worldpay/access/checkout/api/CardBinClientIntegrationTest.kt
@@ -4,8 +4,10 @@ import android.content.Context
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
 import com.github.tomakehurst.wiremock.client.MappingBuilder
+import com.github.tomakehurst.wiremock.client.WireMock
 import com.github.tomakehurst.wiremock.client.WireMock.aResponse
 import com.github.tomakehurst.wiremock.client.WireMock.equalTo
+import com.github.tomakehurst.wiremock.client.WireMock.get
 import com.github.tomakehurst.wiremock.client.WireMock.post
 import com.github.tomakehurst.wiremock.client.WireMock.stubFor
 import com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo
@@ -13,12 +15,15 @@ import com.github.tomakehurst.wiremock.matching.EqualToJsonPattern
 import com.worldpay.access.checkout.api.MockServer.getBaseUrl
 import com.worldpay.access.checkout.api.MockServer.startWiremock
 import com.worldpay.access.checkout.api.MockServer.stopWiremock
+import com.worldpay.access.checkout.api.configuration.CardConfigurationClient
 import com.worldpay.access.checkout.cardbin.api.client.CardBinClient
 import com.worldpay.access.checkout.cardbin.api.client.WP_API_VERSION
 import com.worldpay.access.checkout.cardbin.api.client.WP_API_VERSION_VALUE
 import com.worldpay.access.checkout.cardbin.api.client.WP_CALLER_ID
 import com.worldpay.access.checkout.cardbin.api.client.WP_CALLER_ID_VALUE
 import com.worldpay.access.checkout.cardbin.api.request.CardBinRequest
+import com.worldpay.access.checkout.cardbin.api.serialization.CardBinResponseDeserializer
+import com.worldpay.access.checkout.client.api.exception.AccessCheckoutException
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
 import org.junit.After
@@ -27,15 +32,17 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import kotlin.test.assertEquals
+import kotlin.test.fail
 
 @ExperimentalCoroutinesApi
 @RunWith(AndroidJUnit4::class)
 class CardBinClientIntegrationTest {
     private val cardBinEndpoint = "public/card/bindetails"
+    private val cardNumber = "444433332222"
+    private val checkoutId = "some-checkout-id"
 
     @get:Rule
     var coroutinesTestRule = CoroutineTestRule()
-
 
     private val applicationContext: Context = getInstrumentation().context.applicationContext
 
@@ -51,15 +58,11 @@ class CardBinClientIntegrationTest {
 
     @Test
     fun givenValid12DigitPan_shouldReturnSuccessfulResponse() = runBlocking {
-        val cardNumber = "444433332222"
-        val checkoutId = "some-checkout-id"
-
-        val cardBinRequest = """{
+        val request = """{
                 "cardNumber": "$cardNumber",
                 "checkoutId": "$checkoutId"
                 }
                 """
-
         val response =
             """
                       {
@@ -73,7 +76,7 @@ class CardBinClientIntegrationTest {
 
 
         stubFor(
-            postRequest(cardBinRequest)
+            postRequest(request)
                 .willReturn(
                     aResponse()
                         .withStatus(200)
@@ -82,10 +85,7 @@ class CardBinClientIntegrationTest {
                 )
         )
 
-        val assertResponse = false
-
         val cardBinClient = CardBinClient(getBaseUrl())
-
         val cardBinReq =
             CardBinRequest(
                 cardNumber = cardNumber,
@@ -93,7 +93,39 @@ class CardBinClientIntegrationTest {
             )
 
         val cardBinResponse = cardBinClient.getCardBinResponse(cardBinReq)
-        assertEquals(response, cardBinResponse.toString())
+        val expectedResponse = CardBinResponseDeserializer().deserialize(response)
+
+        assertEquals(expectedResponse, cardBinResponse)
+    }
+
+    @Test
+    fun shouldThrowExceptionWhenFailingToGetCardScheme() = runBlocking {
+        val request = """{
+                "cardNumber": "$cardNumber",
+                "checkoutId": "$checkoutId"
+                }
+                """
+
+        stubFor(
+            postRequest(request)
+                .willReturn(
+                    aResponse()
+                        .withStatus(500)
+                )
+        )
+        val cardBinReq =
+            CardBinRequest(
+                cardNumber = cardNumber,
+                checkoutId = checkoutId
+            )
+
+        try {
+            val cardBinClient = CardBinClient(getBaseUrl())
+            cardBinClient.getCardBinResponse(cardBinReq)
+            fail("Expected exception was not thrown")
+        } catch (ace: AccessCheckoutException) {
+            assertEquals("There was an error when trying to get card schemes", ace.message)
+        }
     }
 
     private fun postRequest(request: String): MappingBuilder {
---
+++ access-checkout/src/test/java/com/worldpay/access/checkout/cardbin/serialization/CardBinRequestSerializerTest.kt
@@ -0,0 +1,38 @@
+package com.worldpay.access.checkout.cardbin.serialization
+
+import com.worldpay.access.checkout.api.serialization.Serializer
+import com.worldpay.access.checkout.cardbin.api.request.CardBinRequest
+import com.worldpay.access.checkout.cardbin.api.serialization.CardBinRequestSerializer
+import com.worldpay.access.checkout.testutils.PlainRobolectricTestRunner
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(PlainRobolectricTestRunner::class)
+class CardBinRequestSerializerTest {
+
+    private val cardBinSerializer: Serializer<CardBinRequest> = CardBinRequestSerializer()
+
+    @Test
+    fun `should be able to serialize card bin api request to json`() {
+        val cardBinRequest =
+            CardBinRequest(
+                cardNumber = "444433332222",
+                checkoutId = "checkout"
+            )
+
+        val expectedRequest = """
+                {
+                    "cardNumber": "444433332222",
+                    "checkoutId": "checkout"
+                }"""
+
+        val serializedJsonRequest = cardBinSerializer.serialize(cardBinRequest)
+
+        assertEquals(removeWhitespace(expectedRequest), removeWhitespace(serializedJsonRequest))
+    }
+
+    private fun removeWhitespace(string: String): String {
+        return string.replace("\\s".toRegex(), "")
+    }
+}
