d51254a3190b5ddae57479a961e9fc0d509e72e2
Michael McFeat <michael.mcfeat@worldpay.com>
2025-05-30 12:29:57 +0100

    US2126253: Changing WP_CALLER_ID_VALUE to be set to checkoutandroid to be more descriptive

--- access-checkout/src/main/java/com/worldpay/access/checkout/cardbin/api/client/Headers.kt
+++ access-checkout/src/main/java/com/worldpay/access/checkout/cardbin/api/client/Headers.kt
@@ -3,5 +3,5 @@ package com.worldpay.access.checkout.cardbin.api.client
 internal const val WP_API_VERSION = "WP-Api-Version"
 internal const val WP_API_VERSION_VALUE = "1"
 internal const val WP_CALLER_ID = "WP-CallerId"
-internal const val WP_CALLER_ID_VALUE = "checkout"
+internal const val WP_CALLER_ID_VALUE = "checkoutandroid"
 
