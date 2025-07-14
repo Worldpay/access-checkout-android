969685fe976f53edc3f7eae866cd802b63d04a39
Alejandro Baeza Gutierrez <alejandro.baezagutierrez@worldpay.com>
2025-07-14 09:57:14 +0100

    US2123827: Update test

--- demo-app/src/androidTest/java/com/worldpay/access/checkout/sample/navigation/NavigationUITest.kt
+++ demo-app/src/androidTest/java/com/worldpay/access/checkout/sample/navigation/NavigationUITest.kt
@@ -7,14 +7,21 @@ import androidx.test.espresso.contrib.DrawerActions.close
 import androidx.test.espresso.contrib.DrawerActions.open
 import androidx.test.espresso.contrib.DrawerMatchers.isClosed
 import androidx.test.espresso.contrib.DrawerMatchers.isOpen
-import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.espresso.matcher.ViewMatchers.hasSibling
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withParent
+import androidx.test.espresso.matcher.ViewMatchers.withTagKey
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.worldpay.access.checkout.sample.MainActivity
 import com.worldpay.access.checkout.sample.R
 import com.worldpay.access.checkout.sample.testutil.UITestUtils.navigateTo
 import com.worldpay.access.checkout.sample.testutil.matchers.EspressoTestMatchers.withDrawable
-import org.hamcrest.CoreMatchers.*
+import org.hamcrest.CoreMatchers.allOf
+import org.hamcrest.CoreMatchers.instanceOf
+import org.hamcrest.CoreMatchers.not
 import org.junit.After
 import org.junit.Rule
 import org.junit.Test
@@ -36,7 +43,7 @@ class NavigationUITest {
      * This is to fix a major flakiness issue with this test suite
      * This practice is not documented but shown on this page https://github.com/operando/NavigationViewActionsSample
      */
-    private val pauseAfterNavigate = 1000L
+    private val pauseAfterNavigate = 2000L
 
     @After
     fun cleanUp() {
@@ -105,6 +112,11 @@ class NavigationUITest {
 
     @Test
     fun shouldBeAbleToNavigateBackToCardFlow_fromRestrictedCardFlow() {
+        // Open the navigation drawer and ensure it is open
+        onView(withId(R.id.drawer_layout))
+            .perform(open())
+            .check(matches(isOpen()))
+        // Navigate to the restricted card flow
         navigateTo(R.id.nav_restricted_card_flow)
         waitFor(pauseAfterNavigate)
 
@@ -113,6 +125,11 @@ class NavigationUITest {
 
         assertToolbarTitle(restrictedCardFlowTitle)
 
+        // Open the navigation drawer again
+        onView(withId(R.id.drawer_layout))
+            .perform(open())
+            .check(matches(isOpen()))
+        // Navigate back to the card flow
         navigateTo(R.id.nav_card_flow)
         waitFor(pauseAfterNavigate)
 
